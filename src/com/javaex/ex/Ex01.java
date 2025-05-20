8d386b9a9fe3401838f538d46918109596ec392d
kangsb-010313 <kangsb668@naver.com>
2025-05-20 17:23:37 +0900

    pb 250520 17:22 검색기능구현해야됨

--- src/com/javaex/ex/Ex01.java
+++ src/com/javaex/ex/Ex01.java
@@ -22,9 +22,6 @@ public class Ex01 {
 		Reader fr = new FileReader("/Users/kangsubin/Desktop/javaStudy/PhoneDB.txt");
 		BufferedReader br = new BufferedReader(fr);
 		
-		Scanner sc = new Scanner(System.in);
-		boolean run = true;
-		
 		System.out.println("******************************************");
 		System.out.println("*" + "\t" + "전화번호 관리 프로그램" + "\t\t *");
 		System.out.println("******************************************");
@@ -48,6 +45,9 @@ public class Ex01 {
 
 		}
 
+		Scanner sc = new Scanner(System.in);
+		boolean run = true;
+		
 		while(run) {
 			System.out.println("");
 			System.out.println("1.리스트  2.등록  3.삭제  4.검색  5.종료");
@@ -58,7 +58,7 @@ public class Ex01 {
 			
 			switch(menuNo) {
 			
-				case 1: //리스트 출력 (등록, 삭제 할 때마다 없데이트 돼서 출력 돼야함)
+				case 1: //리스트 출력 => 등록, 삭제 할 때마다 없데이트 돼서 출력 돼야함
 					System.out.println("<1.리스트>");	
 					
 					for(int i=0; i<pList.size(); i++) {
@@ -67,7 +67,8 @@ public class Ex01 {
 					}
 					break;
 					
-				case 2: //등록 완료 시 저장 기능
+					
+				case 2: //등록 => 완료 시 저장 기능
 					System.out.println("<2.등록>");
 					sc.nextLine();
 					System.out.print(">이름: ");
@@ -76,7 +77,6 @@ public class Ex01 {
 					String signHp = sc.nextLine();
 					System.out.print(">회사전화: ");
 					String signCompany = sc.nextLine();
-					
 					
 					//리스트 추가///////////////////////////////////
 					Person newP = new Person(signName, signHp, signCompany);
@@ -98,18 +98,20 @@ public class Ex01 {
 					bw.close();
 					break;
 					
-				case 3: //입력 번호 정보 삭제 기능
+					
+				case 3: //삭제 => 입력 번호 정보 삭제 기능
 					System.out.println("<3.삭제>");
 					System.out.print(">번호: ");
 					int deleteNo = sc.nextInt(); //삭제할 번호 입력 
 					
 					// remove 
-					if(deleteNo > 0 && deleteNo <= pList.size()) { //(1 ~ pList.size())
+					if(deleteNo > 0 && deleteNo <= pList.size()) { // 삭제 입력 가능 번호 => 1 ~ pList.size()
 					//방은 0부터인데,,,, 흠
-					//deleteNo = i => 1=0 2=1 3=2 4=3 1씩 차이남
+					//deleteNo=i --> 1=0 2=1 3=2 4=3 1씩 차이남
 						pList.remove(deleteNo - 1); //유레카
+						System.out.println("[삭제되었습니다.]");
 			
-					}else {
+					}else { // 번호 잘못 입력했을 때 
 						System.out.println("[번호를 다시 입력해 주세요.]");
 					}
 
@@ -124,16 +126,16 @@ public class Ex01 {
 						removebw.flush();
 					}
 					
-					System.out.println("[삭제되었습니다.]");
-					
 					removebw.close();
 					break;
 					
-				case 4: //검색 기능: 키워드(이름) 입력시 해당 리스트 출력 기능
+					
+				case 4: //검색 => 키워드(이름) 입력시 해당 리스트 출력 기능
 					System.out.println("<4.검색>");
 					System.out.print(">이름: ");
 					String searchName = sc.nextLine();
 					break;
+					
 					
 				case 5: //종료
 					System.out.println("");
